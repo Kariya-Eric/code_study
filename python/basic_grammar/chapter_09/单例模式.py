8dc2c84a47f60d76db13bd0b86786c1e434bb033
kariya <466636883@qq.com>
2025-03-24 18:03:48 +0800

    2025年3月24日 18:03:45

--- python/basic_grammar/chapter_09/单例模式.py
+++ python/basic_grammar/chapter_09/单例模式.py
@@ -26,16 +26,77 @@ my_instance2 = MyInstance()
 print(my_instance, my_instance1, my_instance2)
 
 
+# 模块的导入也是单例模式
+# __new__ 单例模式
 class Test:
-    def __new__(cls):
-        print("__new__方法..")
-        print("调用父类的方法创建实例对象")
-        ins = super().__new__(cls)
-        print("ins:", ins)
-        return ins
+    __singleton_instance = None
 
-    def __init__(self):
-        print("__init__方法:", self)
+    def __new__(cls, *args, **kwargs):
+        if Test.__singleton_instance is None:
+            Test.__singleton_instance = object.__new__(cls)
+        return Test.__singleton_instance
+
+    def __init__(self, name):
+        print("..init", self)
+        self.name = name
 
 
-t1 = Test()
+t1 = Test("zhangsan")
+t2 = Test("lisi")
+print(t1 is t2, t1.name, t2.name)
+
+
+# 类方法
+class Singleton2:
+    __instance = None
+
+    @classmethod
+    def get_instance(cls):
+        if Singleton2.__instance is None:
+            Singleton2.__instance = object.__new__(cls)
+        return Singleton2.__instance
+
+
+s1 = Singleton2.get_instance()
+s2 = Singleton2.get_instance()
+print(s1, s2, s1 is s2)
+
+
+# 装饰器生成单例模式
+def outer(fn):
+    _ins = {}
+
+    def inner(*args, **kwargs):
+        if fn not in _ins:
+            _ins[fn] = fn(*args, **kwargs)
+            print(fn, _ins, _ins[fn])
+        return _ins[fn]
+
+    return inner
+
+
+@outer
+class Singleton3:
+    pass
+
+
+s31 = Singleton3()
+s32 = Singleton3()
+print(s31 is s32)
+
+
+# 使用元类实现单例模式
+class Singleton4:
+    def __init__(self, name):
+        self.name = name
+
+    def __new__(cls, *args, **kwargs):
+        # 对象不存在_ins属性
+        if not hasattr(cls, '_ins'):
+            cls._ins = object.__new__(cls)
+        return cls._ins
+
+
+s41 = Singleton4("张三")
+s42 = Singleton4("李四")
+print(s41 is s42, s41.name, s42.name)
